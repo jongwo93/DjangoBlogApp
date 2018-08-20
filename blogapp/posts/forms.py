50b2d1640afb272730797dd6a0fcabda402e2aa0
Jongwoo <jongwo93@gmail.com>
2018-08-21 02:36:34 +0900

    searchable, filter(staffonly), markdown implemented.

--- blogapp/posts/forms.py
+++ blogapp/posts/forms.py
@@ -1,12 +1,16 @@
 from django import forms
-
+from pagedown.widgets import PagedownWidget
 from .models import Post
 
 class PostForm(forms.ModelForm):
+    content = forms.CharField(widget=PagedownWidget)
+    publish = forms.DateTimeField(widget=forms.SelectDateWidget)
     class Meta:
         model = Post
         fields = [
             "title",
             "content",
             "image",
+            "draft",
+            "publish",
         ]
--- blogapp/posts/views.py
+++ blogapp/posts/views.py
@@ -2,17 +2,24 @@ from urllib.parse import quote_plus
 
 from django.contrib import messages
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 # Create your views here.
 from .forms import PostForm
 from .models import Post
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-
+from django.utils import timezone
+from django.db.models import Q
 
 def post_create(request):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
+    # if not request.is_authenticated():
+    #     raise Http404
+
     form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
+        instance.user = request.user
         instance.save()
 
         messages.success(request, "Successfully Created")
@@ -29,6 +36,9 @@ def post_create(request):
 def post_detail(request, slug=None):
     #instance = Post.objects.get(id=2)
     instance = get_object_or_404(Post, slug=slug) #title="Post 3"
+    if instance.draft or instance.publish > timezone.now().date():
+        if not request.user.is_staff or not request.user.is_superuser:
+            raise Http404
     share_string = quote_plus(instance.content)
     context = {
         "title": instance.title,
@@ -46,9 +56,21 @@ def post_list(request): # list items
     #     context = {
     #         "title": "List"
     #     }
-    queryset_list = Post.objects.all()
-    paginator = Paginator(queryset_list, 5)  # Show 25 contacts per page
-    page_request_var = "abc"
+    today = timezone.now().date()
+    queryset_list = Post.objects.active()
+    if request.user.is_staff or request.user.is_superuser:
+        queryset_list = Post.objects.all()
+
+    query = request.GET.get("q")
+    if query:
+        queryset_list = queryset_list.filter(
+            Q(title__icontains=query) |
+            Q(content__icontains=query) |
+            Q(user__first_name__icontains=query) |
+            Q(user__last_name__icontains=query)
+        ).distinct()
+    paginator = Paginator(queryset_list, 3)  # Show 25 contacts per page
+    page_request_var = "page"
     page = request.GET.get(page_request_var)
 
     try:
@@ -62,11 +84,14 @@ def post_list(request): # list items
         "object_list" : queryset,
         "title": "List",
         "page_request_var": page_request_var,
+        "today": today,
     }
     return render(request, "post_list.html", context)
 
 
 def post_update(request, slug=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, slug=slug)
     form = PostForm(request.POST or None, request.FILES or None, instance = instance)  #instance = instance ; it adds original data to edit form
     if form.is_valid():
@@ -85,6 +110,8 @@ def post_update(request, slug=None):
 
 
 def post_delete(request, slug=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, slug=slug)
     messages.success(request, "Successfully Updated", extra_tags="some-tag")
     return redirect("posts:list")
